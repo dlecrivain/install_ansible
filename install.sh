18f5e4dd73f53767d37a5b3aa40d800c01462068
Daniel LECRIVAIN <d.lecrivain47@gmail.com>
2022-02-27 17:43:40 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt install python3-pip
+sudo pip3 install pywinrm
+sudo pip3 install pyvmomi
+sudo pip3 install ansible
